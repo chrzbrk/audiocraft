6de7b85ac804b07ddb4411f7f4a1ee19296db8e0
chrzbrk <156067510+chrzbrk@users.noreply.github.com>
2024-10-01 00:50:08 +0200

    Update generate_music.py

--- generate_music.py
+++ generate_music.py
@@ -3,13 +3,13 @@ from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
 
 # Lade das vortrainierte Modell (z.B. musicgen-medium)
-model = MusicGen.get_pretrained('facebook/musicgen-medium')
+model = MusicGen.get_pretrained('facebook/musicgen-large')
 
 # Setze die Generierungsparameter (z.B. 8 Sekunden Musik)
-model.set_generation_params(duration=8)
+model.set_generation_params(duration=15)
 
 # Generiere eine Musik basierend auf einer Textbeschreibung
-descriptions = ['relaxing ambient music', 'energetic EDM', 'classical piano piece']
+descriptions = ['cinematic, dark, instrumental music']
 generated_audio = model.generate(descriptions)
 
 # Speichere die generierten Audiodateien
